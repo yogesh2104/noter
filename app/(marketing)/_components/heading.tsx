5376b09f6ea45d455966939da10eff13f53d00d4
yogesh2104 <singhyogesh2104@gmail.com>
2023-11-28 08:36:32 +0530

    initial commit

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const Heading = () => {
+    return ( 
+        <div className="max-w-3xl space-y-4">
+            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Noter</span></h1>
+        </div>
+     );
+}
+ 
+export default Heading;
